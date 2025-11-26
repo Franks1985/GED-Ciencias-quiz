d77c036cf5edc7d0a28d527dda6624cdd284feaa
Francisco Sequera <86817814+Franks1985@users.noreply.github.com>
2025-11-25 20:21:37 -0500

    Add service worker for GED Ciencias 2025

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js – Service Worker para GED Ciencias 2025
+const CACHE_NAME = 'ged-ciencias-v1.2';
+const urlsToCache = [
+  '/',
+  '/GED-Ciencias-quiz/',
+  '/GED-Ciencias-quiz/index.html',
+  '/GED-Ciencias-quiz/manifest.json',
+  '/GED-Ciencias-quiz/icon-192.png',
+  '/GED-Ciencias-quiz/icon-512.png'
+  // Puedes agregar más si quieres, pero con esto ya funciona perfecto
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
